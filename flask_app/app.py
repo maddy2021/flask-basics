2c9556807b872ab4260e880058ddcb22eca66225
maddy2021 <45945046+maddy2021@users.noreply.github.com>
2022-01-03 23:03:47 +0530

    Added home and about routes

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import date, datetime
 from flask import Flask
 from datetime import datetime
 
@@ -25,6 +25,24 @@ def greetings(my_name):
 @app.route("/square/<int:number>")
 def square(number):
     return f'square of {number} is {number*number}'
+
+@app.route("/home")
+def home():
+    return "Paws Rescue Center 🐾"
+
+@app.route("/about")
+def about():
+    return '''We are a non-profit organization working as an animal rescue.\
+     We aim to help you connect with the purrfect furbaby for you! The animals you find on our website are rescued and \
+     rehabilitated animals. Our mission is to promote the ideology "adopt, don't hop"! .'''
+
+@app.route("/age/<int:year>")
+def calculate_age(year):
+    today= date.today()
+    if year>today.year: return "OOps, you havent born yet, try else"
+    age= today.year- year
+    return "age is " +str(age)
+
 
 if __name__=="__main__":
     app.run(port=3030, debug=True)
